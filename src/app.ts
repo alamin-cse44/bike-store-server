f84a6e61421673a4b4e3c9c5cc7f8c1cf0c20a9a
alamin-cse44 <alamincse44@gmail.com>
2024-11-22 15:57:28 +0600

    Add: simple create & get

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
 import express, { Application, Request, Response } from 'express';
 const app: Application = express();
 import cors from 'cors';
+import { BikeRoutes } from './app/modules/bike/bike.route';
 
 // parser
 app.use(express.json());
 app.use(cors());
+
+// application routing 
+app.use('/api/v1/bikes', BikeRoutes)
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
